05a3e28d6f6d9801e3b5e451a7d21834b329a9b9
prkshayush <itsnotmymailid140@gmail.com>
2025-03-03 01:33:43 +0530

    minor additions

---
+++ spai_app/app/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function PrivacyPage() {
+    return (
+        <div className="text-xl font-semibold text-center mx-10 my-12">
+            <h1>Privacy Policy</h1>
+            <p>Our privacy policy is simple: we don't collect any information about you at all.</p>
+        </div>
+    )
+}
---
+++ spai_app/app/terms/page.tsx
@@ -0,0 +1,14 @@
+export default function Terms() {
+    return (
+        <div className="text-center flex flex-col items-center justify-center text-xl mt-5">
+            <h1 className="text-3xl">Terms of Service</h1>
+            <p>
+                By using our services, you agree to the following terms:
+            </p>
+            <ul>
+                <li>You must be at least 18 years old to use our services</li>
+                <li>The developer of this web app doesnot guarentee the predictions, please consult a health specialist. </li>
+            </ul>
+        </div>
+    )
+}
